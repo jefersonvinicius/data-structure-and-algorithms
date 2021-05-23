38fa86301ae3b8971b2f09bf40cf36caa66d147e
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-05-23 18:24:09 -0300

    Add infix to postfix utils functions

--- stack/03-infix-to-posfix.c
+++ stack/03-infix-to-posfix.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct StackNode {
     char value;
@@ -31,14 +32,89 @@ void push(struct StackNode** rootNode, char value) {
 char top(struct StackNode *rootNode) {
     if (is_empty(rootNode)) {
         printf("ERROR ON TOP: empty stack\n");
-        return '-';
+        exit(EXIT_FAILURE);    
     }
     return rootNode->value;
 }
 
+char pop(struct StackNode **rootNode) {
+    if (is_empty(*rootNode)) {
+        printf("ERROR ON POP: empty stack\n");
+        exit(EXIT_FAILURE);    
+    }
+
+    char valuePopped = (*rootNode)->value;
+    *rootNode = (*rootNode)->next;
+    return valuePopped;
+}
+
+int char_is_letter(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+int char_is_digit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+
+void append_char(char *str, char c) {
+    int newSize = (strlen(str) + 2); 
+    str = (char *) realloc(str, newSize);
+    char cater[] = {c};
+    strcat(str, cater);
+}
+
+// const char* pop_all_stack_and_get_as_string(struct StackNode *stack) {
+//     char *stack_content = malloc(0);
+//     while (!is_empty(&stack)) {
+//         append_char(stack_content, pop(&stack));
+//     }
+//     strrev(stack_content);
+//     return stack_content;
+// }
+
+void pop_until_last_left_parentesis(struct StackNode *stack, char *post_fix) {
+    char *content = malloc(0);
+    while (!is_empty(stack)) {
+        char popped = pop(&stack);
+        if (popped == '(') break;
+        append_char(post_fix, popped);
+    }
+}
+
+const char* infix_to_postfix(const char* infixExpression) {
+    char *postFix = malloc(0);
+    struct StackNode *stack = NULL;
+    
+    for (int i = 0; i < strlen(infixExpression); i++) {
+        char current_char = infixExpression[i];
+        if (char_is_digit(current_char) || char_is_letter(current_char)) {
+            append_char(postFix, current_char);
+        } else if (current_char == '(') {
+            push(&stack, current_char);
+        } else if (current_char == ')') {
+            pop_until_last_left_parentesis(stack, postFix);
+        } else {
+            
+        }
+    }
+
+    return postFix;
+}
+
 int main() {
-    struct StackNode* stack = NULL;
-    push(&stack, 'a');
-    push(&stack, 'b');
-    printf("TOP: %c\n", top(stack));
+
+    // char expression1[] = "a-b";
+    char expression2[] = "a-b*c*****+++++++-------****/////////";
+    // char expression3[] = "(a-b)*c";
+    // char expression4[] = "a+b*c^d-e";
+    // char expression5[] = "a*(b+c)*(d-g)*h";
+    // char expression6[] = "a*b-c*d^e/f+g*h";
+
+    // printf("infix: %s - postfix: %s\n", expression1, infix_to_postfix(expression1));
+    printf("infix: %s - postfix: %s\n", expression2, infix_to_postfix(expression2));
+    // printf("infix: %s - postfix: %s\n", expression3, infix_to_postfix(expression3));
+    // printf("infix: %s - postfix: %s\n", expression4, infix_to_postfix(expression4));
+    // printf("infix: %s - postfix: %s\n", expression5, infix_to_postfix(expression5));
+    // printf("infix: %s - postfix: %s\n", expression6, infix_to_postfix(expression6));
 }
