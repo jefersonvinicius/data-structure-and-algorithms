b148136d32d2782072b1b428bdfb0cbe43ee73d7
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2022-04-04 21:11:25 -0300

    Add binary tree inorder and preorder functions

--- binary-tree/main.c
+++ binary-tree/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
+#include "../_utils_/asserts.h"
 
 #include "tree.h"
 
@@ -25,6 +26,59 @@ int main() {
         assert(tree->root->value == 0);
         assert(tree->root->right->value == 1);
         assert(tree->root->left->value == 2);
+    }
+
+    { // should get size correctly
+        struct Tree* tree = create_binary_tree();
+        tree->root = create_node(0);
+        tree->root->right = create_node(1);
+        tree->root->left = create_node(2);
+        tree->root->left->left = create_node(3);
+        tree->root->left->right = create_node(10);
+        tree->root->right->left = create_node(11);
+        assert(tree_size(tree) == 6);
+    }
+
+    { // should get in order transversal correctly
+        struct Tree* tree = create_binary_tree();
+        tree->root = create_node(1);
+        tree->root->left = create_node(2);
+        tree->root->right = create_node(3);
+        tree->root->left->left = create_node(4);
+        tree->root->left->right = create_node(5);
+        tree->root->right->left = create_node(6);
+
+        int* result = in_order(tree);
+        int expected[] = {4, 2, 5, 1, 6, 3};
+        assertArray(result, expected, 6);
+    }
+
+    { // should get pre order transversal correctly
+        struct Tree* tree = create_binary_tree();
+        tree->root = create_node(1);
+        tree->root->left = create_node(2);
+        tree->root->right = create_node(3);
+        tree->root->left->left = create_node(4);
+        tree->root->left->right = create_node(5);
+        tree->root->right->left = create_node(6);
+
+        int* result = pre_order(tree);
+        int expected[] = {1, 2, 4, 5, 3, 6};
+        assertArray(result, expected, 6);
+    }
+
+    { // should get post order transversal correctly
+        struct Tree* tree = create_binary_tree();
+        tree->root = create_node(1);
+        tree->root->left = create_node(2);
+        tree->root->right = create_node(3);
+        tree->root->left->left = create_node(4);
+        tree->root->left->right = create_node(5);
+        tree->root->right->left = create_node(6);
+
+        int* result = post_order(tree);
+        int expected[] = {4, 5, 2, 6, 3, 1};
+        assertArray(result, expected, 6);
     }
 
     return 0;
--- binary-tree/tree.h
+++ binary-tree/tree.h
@@ -22,3 +22,66 @@ struct Tree* create_binary_tree() {
     struct Tree* tree = (struct Tree*) malloc(sizeof(struct Tree*));
     return tree; 
 }
+
+void _count(struct Node* node, int* result) {
+    if (node == NULL) return;
+    
+    (*result)++;
+    _count(node->left, result);
+    _count(node->right, result);
+}
+
+int tree_size(struct Tree* tree) {
+    int size = 0;
+    struct Node* node = tree->root;
+    _count(node, &size);
+    return size;
+}
+
+void _in_order_recursion(struct Node* root, int* result, int* index) {  
+    if (root == NULL) return;
+    
+    _in_order_recursion(root->left, result, index);
+    result[(*index)] = root->value;
+    (*index)++;
+    _in_order_recursion(root->right, result, index);
+}
+
+int* in_order(struct Tree* tree) {
+    int index = 0;
+    int* result = malloc(sizeof(int) * tree_size(tree));
+    _in_order_recursion(tree->root, result, &index);
+    return result;
+}
+
+void _pre_order_recursion(struct Node* root, int* result, int* index) {  
+    if (root == NULL) return;
+    
+    result[(*index)] = root->value;
+    (*index)++;
+    _pre_order_recursion(root->left, result, index);
+    _pre_order_recursion(root->right, result, index);
+}
+
+int* pre_order(struct Tree* tree) {
+    int index = 0;
+    int* result = malloc(sizeof(int) * tree_size(tree));
+    _pre_order_recursion(tree->root, result, &index);
+    return result;
+}
+
+void _post_order_recursion(struct Node* root, int* result, int* index) {  
+    if (root == NULL) return;
+    
+    _post_order_recursion(root->left, result, index);
+    _post_order_recursion(root->right, result, index);
+    result[(*index)] = root->value;
+    (*index)++;
+}
+
+int* post_order(struct Tree* tree) {
+    int index = 0;
+    int* result = malloc(sizeof(int) * tree_size(tree));
+    _post_order_recursion(tree->root, result, &index);
+    return result;
+}
