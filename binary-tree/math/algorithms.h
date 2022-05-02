fa2a5385376e7922dee6693aa82e0b578e3c9e90
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2022-05-02 20:14:59 -0300

    Almost got it the infix_to_postfix

--- binary-tree/math/algorithms.h
+++ binary-tree/math/algorithms.h
@@ -7,42 +7,38 @@ int _char_is_letter(char c) {
 
 
 int _get_operator_precedence(char operator) {
+    // printf("OPERATOR: %c %d\n", operator, operator == '+');
     if (operator == '+' || operator == '-') return 1;
     if (operator == '*' || operator == '/') return 2;
     if (operator == '^') return 3;
     return -1;
 }
 
-void _append_str(char *post_fix[], char *c, int* current_postfix_index) {
-    // int new_size = (strlen(str) + 2); 
-    // str = (char *) realloc(str, new_size);
-    // char cater[] = {c};
-    printf("Apeendng: \n");
-    post_fix[*current_postfix_index] = c;
+void _append_str(char *post_fix[], char *to_append, int* current_postfix_index) {
+    printf("Apeending: %s\n", to_append);
+    post_fix[*current_postfix_index] = to_append;
     (*current_postfix_index)++;
 }
 
 void _pop_until_last_left_parentesis(struct StackNode **stack, char *post_fix[], int *current_postfix_index) {
     while (!is_empty(*stack)) {
         char* popped = pop(stack);
-        if (*popped == '(') break;
+        printf("Popped: %s\n", popped);
+        if (strcmp(popped, "(") == 0) break;
         _append_str(post_fix, popped, current_postfix_index);
     }
 }
 
 void _pop_until_find_operator_have_less_precedence_than_current_operator_and_push_it(char current_operator, struct StackNode** stack, char *post_fix[], int *current_postfix_index) {
     int current_operator_precedence = _get_operator_precedence(current_operator);
-    printf("AQui3\n");
     while (!is_empty(*stack) && current_operator_precedence <= _get_operator_precedence(*top(*stack))) {
-    printf("AQui4\n");
-
         _append_str(post_fix, pop(stack), current_postfix_index);
     }
     push(stack, current_operator);
 }
 
 char* _extract_number_digits(const char* str, int start_index) {
-    char* result;
+    char* result = malloc(0);
     int index = start_index;
     while (isdigit(str[index])) {
         char digit[] = {str[index]};
@@ -61,7 +57,6 @@ char** infix_to_postfix(const char* infix_expression) {
     for (int i = 0; i < strlen(infix_expression); i++) {
         post_fix[i] = malloc(STRING_MAX * sizeof(char));
     }
-    printf("oi\n");
     struct StackNode *stack = NULL;
     
     for (int i = 0; i < strlen(infix_expression); i++) {
@@ -69,21 +64,28 @@ char** infix_to_postfix(const char* infix_expression) {
         if (current_char == ' ') continue;
 
         if (_char_is_letter(current_char)) {
+            printf("%c is letter\n", current_char);
             _append_str(post_fix, &current_char, &postfix_index);
         } else if (isdigit(current_char)) {
-            _append_str(post_fix, _extract_number_digits(infix_expression, i), &postfix_index);
+            printf("%c is digit\n", current_char);
+            char* extracted = _extract_number_digits(infix_expression, i);
+            i += (strlen(extracted) - 1);
+            printf("extracted: %s len(%ld)\n", extracted, strlen(extracted));
+            _append_str(post_fix, extracted, &postfix_index);
         } else if (current_char == '(') {
             push(&stack, current_char);
         } else if (current_char == ')') {
-            printf("Aqui1\n");
+            // printf("Aqui1\n");
+            printf("START Popping\n");
             _pop_until_last_left_parentesis(&stack, post_fix, &postfix_index);
+            printf("END Popping\n");
         } else {
-            printf("Aqui2\n");
+            // printf("Aqui2\n");
             _pop_until_find_operator_have_less_precedence_than_current_operator_and_push_it(current_char, &stack, post_fix, &postfix_index);   
         }
     }
 
-    printf("Saiu\n");
+    // printf("Saiu\n");
 
     while (!is_empty(stack)) {
         _append_str(post_fix, pop(&stack), &postfix_index);
