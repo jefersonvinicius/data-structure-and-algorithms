88f31a576cdc03c65d4729cee7b4678fc86898a2
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2022-07-05 20:16:52 -0300

    Starting understand the BI Tree

--- trees/binary-indexed-tree/main.c
+++ trees/binary-indexed-tree/main.c
@@ -12,44 +12,69 @@ int main() {
 
     { // should insert first value
         struct BITree* tree = create_binary_indexed_tree();
-        bi_insert(tree, 4);
+        bi_insert(tree, 1);
         assert(tree->values[0] == 0);
-        assert(tree->values[1] == 4);
+        assert(tree->values[1] == 1);
+        assert(tree->size == 2);
+        assert(tree->original[0] == 0);
+        assert(tree->original[1] == 1);
     }
 
     { // should insert second value
         struct BITree* tree = create_binary_indexed_tree();
-        bi_insert(tree, 4);
-        bi_insert(tree, 5);
+        bi_insert(tree, 1);
+        bi_insert(tree, 2);
         assert(tree->values[0] == 0);
-        assert(tree->values[1] == 4);
-        assert(tree->values[2] == 5);
+        assert(tree->values[1] == 1);
+        assert(tree->values[2] == 3);
+        assert(tree->size == 3);
+        assert(tree->original[0] == 0);
+        assert(tree->original[1] == 1);
+        assert(tree->original[2] == 2);
     }
 
     { // should insert third value
         struct BITree* tree = create_binary_indexed_tree();
-        bi_insert(tree, 4);
-        bi_insert(tree, 5);
-        bi_insert(tree, 10);
+        bi_insert(tree, 1);
+        bi_insert(tree, 2);
+        bi_insert(tree, 3);
         assert(tree->values[0] == 0);
-        assert(tree->values[1] == 4);
-        assert(tree->values[2] == 15);
-        assert(tree->values[3] == 10);
+        assert(tree->values[1] == 1);
+        assert(tree->values[2] == 3);
+        assert(tree->values[3] == 3);
+        assert(tree->size == 4);
+        assert(tree->original[0] == 0);
+        assert(tree->original[1] == 1);
+        assert(tree->original[2] == 2);
+        assert(tree->original[3] == 3);
     }
 
-    printf("Bla: ");
-    for (int i = 1; i <= 10; i += i & -i) {
-        printf("%d ", i);
+    { // should insert fourth value
+        struct BITree* tree = create_binary_indexed_tree();
+        bi_insert(tree, 1);
+        bi_insert(tree, 2);
+        bi_insert(tree, 3);
+        bi_insert(tree, 4);
+        assert(tree->values[0] == 0);
+        assert(tree->values[1] == 1);
+        assert(tree->values[2] == 3);
+        assert(tree->values[3] == 3);
+        assert(tree->values[4] == 10);
+        assert(tree->size == 5);
+        assert(tree->original[0] == 0);
+        assert(tree->original[1] == 1);
+        assert(tree->original[2] == 2);
+        assert(tree->original[3] == 3);
+        assert(tree->original[4] == 4);
     }
-    printf("\n");
+
 
 
     { // should get sum until index 5
-        struct BITree* tree = create_binary_indexed_tree();
-        // [1,2,3,4,5,6,7,8]
-        for (int i = 1; i <= 8; i++) bi_insert(tree, i);
-        bi_print(tree);
-        assert(bi_get_sum(tree, 5) == 15);
+        // struct BITree* tree = create_binary_indexed_tree();
+        // for (int i = 1; i <= 8; i++) bi_insert(tree, i);
+        // bi_print(tree);
+        // assert(bi_get_sum(tree, 5) == 15);
     }
 
 
--- trees/binary-indexed-tree/tree.h
+++ trees/binary-indexed-tree/tree.h
@@ -13,20 +13,22 @@ struct BITree* create_binary_indexed_tree() {
     struct BITree* tree = malloc(sizeof(struct BITree*));
     tree->values = malloc(sizeof(int) * MAX_BI_TREE_SIZE);
     tree->original = malloc(sizeof(int) * MAX_BI_TREE_SIZE);
+    memset(tree->values, MAX_BI_TREE_SIZE, 0);
+    memset(tree->original, MAX_BI_TREE_SIZE, 0);
     tree->values[0] = 0;
     tree->original[0] = 0;
-    tree->size = 0;
+    tree->size = 1;
     return tree;
 };
 
 void bi_insert(struct BITree* tree, int value) {
-    int index = 1, n = tree->size + 1;
-    while (index <= n) {
-        tree->values[index] += value;
-        index += index & (-index);
+    int initial_index = tree->size;
+    tree->values[initial_index] = value;
+    tree->original[initial_index] = value;
+    int final_index = initial_index - (initial_index & (-initial_index));
+    for  (int i = initial_index - 1; i > final_index; i--) {
+        tree->values[initial_index] += tree->original[i];
     }
-    
-    tree->original[n] = value;
     tree->size++;
 }
 
@@ -41,13 +43,13 @@ int bi_get_sum(struct BITree* tree, int end) {
 }
 
 int bi_print(struct BITree* tree) {
-    printf("- BITree - \n");
+    printf("- BITree -\n");
     printf("Original: ");
-    for (int i = 0; i <= tree->size; i++) {
+    for (int i = 0; i < tree->size; i++) {
         printf("%d ", tree->original[i]);
     }
     printf("\nBinary Indexed: ");
-    for (int i = 0; i <= tree->size; i++) {
+    for (int i = 0; i < tree->size; i++) {
         printf("%d ", tree->values[i]);
     }
     printf("\n");
