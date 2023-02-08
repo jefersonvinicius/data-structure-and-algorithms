461dbfd4636c5cc4b069bbf5a48d9c5a7f4e3dbd
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2023-02-07 22:29:42 -0300

    Finished heap_delete function

--- heap/heap.h
+++ heap/heap.h
@@ -3,6 +3,7 @@
 struct Heap {
     int* elements;
     int max_size, current_index, size;
+    int (*cmp)(int, int);
 };
 
 /*
@@ -21,9 +22,16 @@ int _right_child_index(int i) { return 2 * i + 1; }
 int _get_next_child_index(struct Heap* heap, int index) {
     int current = heap->elements[index];
     int right_child_index = _right_child_index(index);
-    if (right_child_index <= heap->current_index && current < heap->elements[right_child_index]) return right_child_index;
     int left_child_index = _left_child_index(index);
-    if (left_child_index <= heap->current_index && current < heap->elements[left_child_index]) return left_child_index;
+    int is_valid_right_child_index = right_child_index <= heap->current_index;
+    int is_valid_left_child_index = left_child_index <= heap->current_index;
+    if (
+        is_valid_right_child_index && 
+        heap->cmp(heap->elements[right_child_index], current)  && 
+        (!is_valid_left_child_index || (is_valid_left_child_index && heap->elements[right_child_index] >= heap->elements[left_child_index]))
+    ) 
+        return right_child_index;
+    if (is_valid_left_child_index && heap->cmp(heap->elements[left_child_index], current)) return left_child_index;
     return -1;
 }
 
@@ -33,18 +41,24 @@ void _swap_elements(int* elements, int index_a, int index_b) {
     elements[index_b] = tmp;
 }
 
-struct Heap* create_heap(int size) {
+
+int max_cmp(int a, int b) {
+    return a > b;
+}
+
+struct Heap* create_heap(int size, int (*cmp)(int, int)) {
     struct Heap* heap = (struct Heap*) malloc(sizeof(struct Heap*));
     heap->max_size = size;
     heap->current_index = 1;
     heap->elements = (int*) malloc(sizeof(int) * size + sizeof(int));
     heap->elements[0] = 0;
+    heap->cmp = cmp;
     return heap;
 }
 
 void heap_insert(struct Heap* heap, int value) {
     int index = heap->current_index;
-    while (index > 1 && value > heap->elements[_parent_index(index)]) {
+    while (index > 1 &&  heap->cmp(value, heap->elements[_parent_index(index)])) {
         heap->elements[index] = heap->elements[_parent_index(index)];
         index = _parent_index(index);
     }
@@ -54,22 +68,23 @@ void heap_insert(struct Heap* heap, int value) {
 
 
 int heap_top(struct Heap* heap) {
+    if (heap->current_index <= 1) {
+        fprintf(stderr, "Heap empty error!\n");
+        exit(-1);
+    };
     return heap->elements[HEAP_ROOT_INDEX];
 }
 
 int heap_delete(struct Heap* heap) {
     int deleted_value = heap_top(heap);
-    printf("DELETING: %d\n", deleted_value);
     int last = heap->elements[--heap->current_index];
     heap->elements[HEAP_ROOT_INDEX] = last;
     int index = HEAP_ROOT_INDEX;
     while (1) {
         int next_index = _get_next_child_index(heap, index);
         if (next_index == -1) break;
-        printf("MOVING %d to %d\n", heap->elements[index], heap->elements[next_index]);
         _swap_elements(heap->elements, index, next_index);
         index = next_index;
     }
-    printf("DELETED: %d\n", deleted_value);
     return deleted_value;
 }
--- heap/tests.c
+++ heap/tests.c
@@ -35,10 +35,28 @@ int main() {
         int values[7] = {5, 15, 10, 30, 35, 40, 25};
         for (int i = 0; i < 7; i++) heap_insert(heap, values[i]);
         assert(heap_delete(heap) == 40);
-        int expected[7] = {0, 35, 30, 25, 15, 10, 5};
+        int expected[7] = {0, 35, 30, 25, 5, 15, 10};
         assert_array(heap->elements, expected, 7);
         assert(heap_delete(heap) == 35);
+        int expected2[7] = {0, 30, 15, 25, 5, 10};
+        assert_array(heap->elements, expected2, 6);
         assert(heap_delete(heap) == 30);
+        int expected3[7] = {0, 25, 15, 10, 5};
+        assert_array(heap->elements, expected3, 5);
+        assert(heap_delete(heap) == 25);
+        assert(heap_delete(heap) == 15);
+        assert(heap_delete(heap) == 10);
+        assert(heap_delete(heap) == 5);   
+        heap_insert(heap, 70);
+        heap_insert(heap, 81);
+        heap_insert(heap, 40);
+        assert(heap_delete(heap) == 81);   
+        assert(heap_delete(heap) == 70);   
+        assert(heap_delete(heap) == 40);   
+    }
+
+    { // min heap
+        
     }
 
     return 0;
