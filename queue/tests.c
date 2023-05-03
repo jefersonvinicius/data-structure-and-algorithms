a6eea5c70e013d80d99b51770ad46c59cd0fdf6f
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2023-05-03 20:10:29 -0300

    Move queue implementation to generics

--- queue/tests.c
+++ queue/tests.c
@@ -16,37 +16,37 @@ int main() {
         assert(*((int*) lklq_front(queue)) == 1);
         lklq_enqueue(queue, intp(2));
         assert(*((int*) lklq_front(queue)) == 1);
-        lklq_enqueue(queue, 3);
+        lklq_enqueue(queue, intp(3));
         assert(*((int*) lklq_front(queue)) == 1);
-        assert(queue->front->next->data == 2);
-        assert(queue->front->next->next->data == 3);
-        lklq_enqueue(queue, 4);
-        assert(queue->front->data == 1);
-        assert(queue->front->next->data == 2);
-        assert(queue->front->next->next->data == 3);
-        assert(queue->front->next->next->next->data == 4);
+        assert(*((int*) queue->front->next->data) == 2);
+        assert(*((int*) queue->front->next->next->data) == 3);
+        lklq_enqueue(queue, intp(4));
+        assert(*((int*) queue->front->data) == 1);
+        assert(*((int*) queue->front->next->data) == 2);
+        assert(*((int*) queue->front->next->next->data) == 3);
+        assert(*((int*) queue->front->next->next->next->data) == 4);
         assert(queue->front->next->next->next->next == NULL);
     }
 
     { // should dequeue correctly
         struct LklQueue *queue = create_lkl_queue();
         assert(queue->front == NULL);
-        lklq_enqueue(queue, 1);
+        lklq_enqueue(queue, intp(1));
         lklq_dequeue(queue);
         assert(queue->front == NULL);
-        lklq_enqueue(queue, 1);
-        lklq_enqueue(queue, 2);
+        lklq_enqueue(queue, intp(1));
+        lklq_enqueue(queue, intp(2));
         lklq_dequeue(queue);
-        assert(queue->front->data == 2);
+        assert(*((int*)queue->front->data) == 2);
         assert(queue->front->next == NULL);
         lklq_dequeue(queue);
         assert(lklq_is_empty(queue) == 1);
-        lklq_enqueue(queue, 1);
-        lklq_enqueue(queue, 2);
-        lklq_enqueue(queue, 3);
+        lklq_enqueue(queue, intp(1));
+        lklq_enqueue(queue, intp(2));
+        lklq_enqueue(queue, intp(3));
         lklq_dequeue(queue);
-        assert(queue->front->data == 2);
-        assert(queue->front->next->data == 3);
+        assert(*((int*)queue->front->data) == 2);
+        assert(*((int*)queue->front->next->data) == 3);
         assert(queue->front->next->next == NULL);
     }
 
